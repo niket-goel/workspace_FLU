2c31b479bfbb9750ef57987bec27249a54954b1f
Nik <niket.goel@live.com>
2014-04-08 22:59:42 -0400

    Added feature to make sure the user has to register once only

--- FluNetwork/src/com/example/flunetwork/helper/MyGlobal.java
+++ FluNetwork/src/com/example/flunetwork/helper/MyGlobal.java
@@ -1,6 +1,10 @@
 package com.example.flunetwork.helper;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
 import com.example.entity.eventendpoint.model.Event;
 import com.example.entity.userendpoint.model.User;
 /**
@@ -14,9 +18,37 @@ public class MyGlobal {
 	public static User currentUser = null;
 	public static Event currentEvent = null;
 
+	public static String userID = null;
+	//private static Boolean Registered; 
 	
 	//Constants
 	public static final double IMPOSSIBLE_LOCATION = 9999.9;
 	// Time between two back button pushes to push app to background in ms
 	public static final double TIME_DIFF = 2000; 
+	public final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+	
+	public final static String IS_REGISTERED_PREF = "isRegistered";
+	
+	public final static int COI_ONE = 9;
+	public final static int COI_TWO = 8;
+	public final static int COI_THREE = 7;
+	public final static int COI_FOUR = 6;
+	public final static int COI_FIVE = 5;
+	/**
+	 * @return the registered
+	 */
+	public static Boolean IsRegistered(Context context) {
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+		Boolean storedPreference = preferences.getBoolean(IS_REGISTERED_PREF, false);
+		return storedPreference;
+	}
+	/**
+	 * @param registered the registered to set
+	 */
+	public static void setRegistered(Context context, Boolean registered) {
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+		SharedPreferences.Editor editor = preferences.edit();
+		editor.putBoolean(IS_REGISTERED_PREF, registered); 
+		editor.commit();
+	}
 }
--- FluNetwork/src/com/example/flunetwork/ui/RegisterActivity.java
+++ FluNetwork/src/com/example/flunetwork/ui/RegisterActivity.java
@@ -1,19 +1,23 @@
 package com.example.flunetwork.ui;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 import com.example.entity.userendpoint.Userendpoint;
-import com.example.entity.userendpoint.model.PhoneNumber;
 import com.example.entity.userendpoint.model.User;
 import com.example.flunetwork.CloudEndpointUtils;
 import com.example.flunetwork.R;
 import com.example.flunetwork.helper.GPSTracker;
 import com.example.flunetwork.helper.MyGlobal;
+import com.github.davidmoten.geo.GeoHash;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.json.jackson.JacksonFactory;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
@@ -28,6 +32,7 @@ public class RegisterActivity extends Activity implements OnClickListener{
 	Button registerBtn;
 	double latitude;
 	double longitude;
+
 	ProgressBar bar;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,13 +42,13 @@ public class RegisterActivity extends Activity implements OnClickListener{
 		registerBtn.setOnClickListener(this);
 		MyGlobal.currentLoc = new GPSTracker(this);	
 		bar = (ProgressBar) this.findViewById(R.id.regprogressBar);
-		
+
 	}
-	
+
 	@Override
 	protected void onResume() {
 		super.onResume();
-		
+
 		if(MyGlobal.currentLoc.canGetLocation())
 		{
 			MyGlobal.currentLoc.getLocation();
@@ -54,8 +59,8 @@ public class RegisterActivity extends Activity implements OnClickListener{
 		{
 			MyGlobal.currentLoc.showSettingsAlert();
 		}
-		
-		if(MyGlobal.currentUser == null)
+
+		if(!MyGlobal.IsRegistered(getApplicationContext()))
 		{
 			registerBtn.setVisibility(Button.VISIBLE);
 		}
@@ -79,71 +84,120 @@ public class RegisterActivity extends Activity implements OnClickListener{
 		if(v == registerBtn)
 		{
 			new CreateUserTask().execute();
-			
+
 		}
 	}
-	
-	
-	/**
-	   * AsyncTask for calling Mobile Assistant API for checking into a place (e.g., a store)
-	   */
-	  private class CreateUserTask extends AsyncTask<Void, Void, Void> {
 
-		  @Override
+
+	/**
+	 * AsyncTask for calling Mobile Assistant API for checking into a place (e.g., a store)
+	 */
+	private class CreateUserTask extends AsyncTask<Void, Void, Void> {
+
+		@Override
 		protected void onPreExecute() {
 			super.onPreExecute();
 			registerBtn.setVisibility(View.INVISIBLE);
 			bar.setVisibility(View.VISIBLE);
 		}
-		  
-		  /**
-	     * Calls appropriate CloudEndpoint to indicate that user checked into a place.
-	     *
-	     * @param params the place where the user is checking in.
-	     */
+
+		/**
+		 * Calls appropriate CloudEndpoint to indicate that user checked into a place.
+		 *
+		 * @param params the place where the user is checking in.
+		 */
 		@Override
-	    protected Void doInBackground(Void... params) {
-		
-	      User newUser = new User();
-	      newUser.setUserName("Niket");
-	      newUser.setPhoneNumber(new PhoneNumber().setNumber("1234567890"));
-	      newUser.setUserLat(MyGlobal.currentLoc.getLatitude());
-	      newUser.setUserLong(MyGlobal.currentLoc.getLongitude());
-	      newUser.setIsPublisher(false);
-	      // TODO push user location
-	      
-	      Userendpoint.Builder builder = new Userendpoint.Builder(
-	          AndroidHttp.newCompatibleTransport(), new JacksonFactory(),
-	          null);
+		protected Void doInBackground(Void... params) {
 
-	      builder = CloudEndpointUtils.updateBuilder(builder);
+			String userHash = GeoHash.encodeHash(MyGlobal.currentLoc.getLatitude(), 
+					MyGlobal.currentLoc.getLongitude(), MyGlobal.COI_ONE);
+			User newUser = new User();
+			String uName = getUsername()!=null ? getUsername() : "anonymous";
+			newUser.setUserName(uName);
+			String uMail = getUsermail()!=null ? getUsermail() : "anonymous@user.com";
+			newUser.setUserEmailID(uMail);
+			newUser.setUserLat(MyGlobal.currentLoc.getLatitude());
+			newUser.setUserLong(MyGlobal.currentLoc.getLongitude());
+			newUser.setUserGeoHash(userHash);
+			newUser.setIsPublisher(false);
+			// TODO push user location
 
-	      Userendpoint endpoint = builder.build();
+			Userendpoint.Builder builder = new Userendpoint.Builder(
+					AndroidHttp.newCompatibleTransport(), new JacksonFactory(),
+					null);
+
+			builder = CloudEndpointUtils.updateBuilder(builder);
+
+			Userendpoint endpoint = builder.build();
 
 
-	      try {
-	        MyGlobal.currentUser = endpoint.insertUser(newUser).execute();
-	      } catch (IOException e) {
-	        e.printStackTrace();
-	      }
+			try {
+				MyGlobal.currentUser = endpoint.insertUser(newUser).execute();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 
-	      return null;
-	    }
+			return null;
+		}
 		@Override
 		protected void onPostExecute(Void result) {
-				// TODO Put some kind of progress bar
+			// TODO Put some kind of progress bar
 			super.onPostExecute(result);
-				if(MyGlobal.currentUser != null)
-				{
-					//bar.setVisibility(View.GONE);
-					startActivity(new Intent(getApplicationContext(),LandingActivity.class));
-				}
-				else
-				{
-					bar.setVisibility(View.GONE);
-					registerBtn.setVisibility(View.VISIBLE);
-					Toast.makeText(getApplicationContext(), "Could not contact server", Toast.LENGTH_SHORT).show();
-				}
-        }
-	  }
+			if(MyGlobal.currentUser != null)
+			{
+				MyGlobal.setRegistered(getApplicationContext(), true);
+				startActivity(new Intent(getApplicationContext(),LandingActivity.class));
+			}
+			else
+			{
+				bar.setVisibility(View.GONE);
+				registerBtn.setVisibility(View.VISIBLE);
+				Toast.makeText(getApplicationContext(), "Could not contact server", Toast.LENGTH_SHORT).show();
+			}
+		}
+	}
+	
+	
+	private String getUsername(){
+	    AccountManager manager = AccountManager.get(this); 
+	    Account[] accounts = manager.getAccountsByType("com.google"); 
+	    List<String> possibleEmails = new LinkedList<String>();
+
+	    for (Account account : accounts) {
+	      // TODO: Check possibleEmail against an email regex or treat
+	      // account.name as an email address only for certain account.type values.
+	      possibleEmails.add(account.name);
+	    }
+
+	    if(!possibleEmails.isEmpty() && possibleEmails.get(0) != null){
+	        String email = possibleEmails.get(0);
+	        String[] parts = email.split("@");
+	        if(parts.length > 0 && parts[0] != null)
+	            return parts[0];
+	        else
+	            return null;
+	    }else
+	        return null;
+	}
+	
+	private String getUsermail(){
+	    AccountManager manager = AccountManager.get(this); 
+	    Account[] accounts = manager.getAccountsByType("com.google"); 
+	    List<String> possibleEmails = new LinkedList<String>();
+
+	    for (Account account : accounts) {
+	      // TODO: Check possibleEmail against an email regex or treat
+	      // account.name as an email address only for certain account.type values.
+	      possibleEmails.add(account.name);
+	    }
+
+	    if(!possibleEmails.isEmpty() && possibleEmails.get(0) != null){
+	        String email = possibleEmails.get(0);
+	        if(email != null)
+	            return email;
+	        else
+	            return null;
+	    }else
+	        return null;
+	}
 }
